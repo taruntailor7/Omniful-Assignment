69e5d0f3447458b51ec9963ab93ede8b926b6752
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-02 12:45:04 +0530

    navbar done

--- omniful/src/Components/Topbar.jsx
+++ omniful/src/Components/Topbar.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import "./Topbar.css"
+import { BsLink, BsChevronDown, BsBell, BsQuestionCircle} from 'react-icons/bs';
+
 
 export const Topbar = () => {
   return (
-    <div className="topbar p-2">
-        <div className="bar d-flex justify-content-between">
-          <div className="quickLink d-flex mt-2">
-            <p className='mt-3'>Quick Links</p>
-          </div>
-          <div>
-            <p>Quick Links</p>
+    <>
+      <div className="topbar p-2">
+          <div className="bar d-flex justify-content-between">
+            <div className="quickLink d-flex mt-2">
+              <BsLink />
+              <p className='mt-3 mx-2'>Quick Links</p>
+              <BsChevronDown fontSize={12}/>
+            </div>
+            <div className="lang d-flex justify-content-around">
+              <img src="https://cdn-icons-png.flaticon.com/512/555/555526.png" alt="usa" />
+              English
+              <BsBell />
+              <BsQuestionCircle className=''/>
+            </div>
           </div>
         </div>
-    </div>
-
+    </>
   )
 }
