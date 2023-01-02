18a432054d763ea23bd64ad4a4f25a7e179990f5
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-02 09:44:51 +0530

    bootstrap installed

--- omniful/src/App.js
+++ omniful/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div>
       Hello
+      <Button>hey</Button>
     </div>
   );
 }
